c9e9cdcc868a6475fb70ec40bf0ef04cefd5ed47
Spandan Kumar <60343774+spandan-kumar@users.noreply.github.com>
2024-09-20 19:54:34 +0530

    database.py

--- app/db/database.py
+++ app/db/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Replace this with your actual database URL
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres.vuvfapysswkzhtfdhjgj:Hp1010hp1010@@aws-0-ap-south-1.pooler.supabase.com:6543/postgres"
+
+SQLALCHEMY_DATABASE_URL = ""
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
